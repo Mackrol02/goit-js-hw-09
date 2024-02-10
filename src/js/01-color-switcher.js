c2ae249bf60a7a0e4032db20d471ea02bb229d5a
Mackrol02 <mkrol7275@gmail.com>
2024-02-10 16:56:55 +0100

    upd zd1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
-//const
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerid = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+startBtn.addEventListener('click', () => {
+  timerid = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerid);
+  startBtn.disabled = false;
+  stopBtn.disabled = false;
+});
